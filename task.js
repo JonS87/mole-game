7b2978581bdb2dd5c265bf1d09b1b3bba700c0a9
Evgeniy Chausov <evgeniy@chausov.ru>
2024-02-26 17:19:41 +0300

    Done HW

---
+++ task.js
@@ -0,0 +1,20 @@
+const deadCounter = document.getElementById('dead');
+const lostCounter = document.getElementById('lost');
+getHole = index => document.getElementById(`hole${index}`);
+
+for (i = 1; i <= 9; i++) {
+  let hole = getHole(i);
+  hole.onclick = () => {
+    let result = hole.className.includes('hole_has-mole');
+    if (result) {
+      deadCounter.textContent = (+deadCounter.textContent) + 1;
+    } else {
+      lostCounter.textContent = (+lostCounter.textContent) + 1;
+    }
+
+    if ((+lostCounter.textContent) === 5 || (+deadCounter.textContent) === 10) {
+      lostCounter.textContent = 0;
+      deadCounter.textContent = 0;
+    }
+  }
+}
